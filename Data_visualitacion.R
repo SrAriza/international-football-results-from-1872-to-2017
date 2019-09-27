10aec4feb0830e108b58db42fd426eb676b06256
Julian Ariza <53215300+SrAriza@users.noreply.github.com>
2019-09-27 10:49:31 +0200

    Add files via upload

--- Data_visualitacion.R
+++ Data_visualitacion.R
@@ -10,10 +10,12 @@ library(ggplot2)
 library(e1071)
 library(tidyr)
 library(readr)
+library(tidyverse)
 
 #----Loading the data----#
 Data <- read.csv("results.csv")
-
+Countries <- read_csv("countries.csv")
+errors <- read_csv2("errors (2).csv")
 
 #Checking the data 
 summary(Data)
@@ -29,16 +31,60 @@ Data$TotalGoals <- Data$home_score + Data$away_score
 
 # define relevant tournaments
 rel_tour <- c("Copa AmÃ©rica","FIFA World Cup qualification", "FIFA World Cup",
-              "UEFA Euro", "UEFA Euro qualification", "FIFA World Cup","Copa AmÃ©rica qualification", 
-              "African Cup of Nations","AFC Asian Cup", "AFC Asian Cup qualification")
+              "UEFA Euro", "UEFA Euro qualification", "FIFA World Cup","Copa AmÃ©rica qualification")
 
 #Making a new variable for major tournaments
 Data %>% 
-  mutate(check = if_else(tournament %in% rel_tour, 1, 0)) %>% 
-  select(check) -> Data$MajorTournament
+  mutate(MajorTournament = if_else(tournament %in% rel_tour, "True", "False")) -> Data
 
-Data$MajorTournament <- as.numeric(Data$MajorTournament)
+Data$MajorTournament <-  as.factor(Data$MajorTournament)
 
 #Visualitacion of goals scored over the years:
-ggplot(Data, aes(x = date, y = TotalGoals, col = MajorTournament)) + geom_point()
+ggplot(Data, aes(x = date, y = TotalGoals, col = MajorTournament)) + geom_bar()
+
+
+#Creating a new variable to find out who won the game
+Data$GoalsDifference <- Data$home_score - Data$away_score
+
+#New dataset only for major tournaments
+Data %>% 
+  filter(MajorTournament == "True" ) -> MajorTournaments
+
+
+
+#Adding the continent to the dataset
+#Creating the dataset only with country name and region
+Countries %>% 
+  select(home_team = name, region) -> SelectedCountries
+
+#adding the countries with different names or errors encountered
+ReadyData <- bind_rows(SelectedCountries, errors)
+
+#Joining the dataset to add region to the dataset
+MajorTournaments %>% 
+  left_join(y = ReadyData, by = "home_team") -> AllCountries
+
+#How i found the errors the first time
+AllCountries %>% 
+  filter(is.na(region)) %>% 
+  distinct(home_team) -> errors
+
+#write.csv(errors, file = "errors.csv")
+
+#Checking with the new dataset for errors
+
+AllCountries %>% 
+  filter(is.na(region)) %>% 
+  distinct(home_team)
+
+
+#Creating a region for away team
+ReadyData %>% 
+  rename( away_team = home_team, region2= region) -> AwayTeam
+
+#Creating a dataset with regions for home and away team
+AllCountries %>% 
+  left_join(y = AwayTeam, by = "away_team") -> HomeAndAway
+
+
 
